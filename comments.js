03260130a376e734eae98bcdc24085bb7d346603
maaz707 <149104986+maaz707@users.noreply.github.com>
2023-11-09 14:42:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,7 +4,7 @@ const http = require('http');
 // Configure the HTTP server to respond with "Hello, World!" to all requests
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+  res.end('Hello, World!');
 });
 
 // Listen on port 3000 and IP address 127.0.0.1
